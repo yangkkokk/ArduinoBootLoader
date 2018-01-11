c8d82587503134df3d70a13c9d56b4ed4a6a7b20
Mieszko Kłusek <m.klusek@wincan.com>
2018-01-11 14:41:41 +0100

    Network settings adjusted to cleverscan network

--- hardware/ariadne/bootloaders/ariadne/net.h
+++ hardware/ariadne/bootloaders/ariadne/net.h
@@ -12,9 +12,9 @@
 #endif
 
 /* Network settings */
-#define IP_ADDR     192,168,1,128
+#define IP_ADDR     10,0,0,97
 #define SUB_MASK    255,255,255,0
-#define GW_ADDR     192,168,1,254
+#define GW_ADDR     10,0,0,99
 #define MAC_ADDR    0xDE,0xAD,0xBE,0xEF,0xFE,0xED
 
 void netInit(void);
